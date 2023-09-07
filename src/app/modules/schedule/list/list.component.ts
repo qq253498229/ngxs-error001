a9fbba46eacd569b8a83c54caea8ab32fe1d5d59
王斌 <253498229@qq.com>
2023-09-07 16:49:54 +0800

    update

--- src/app/modules/schedule/list/list.component.ts
+++ src/app/modules/schedule/list/list.component.ts
@@ -1,12 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { DatePipe } from '@angular/common';
 import { Store } from '@ngxs/store';
 import { CronAction, CronSelector } from '../../../store/cron';
 import { AutoUnsubscribe } from 'ngx-auto-unsubscribe-decorator';
 import { Observable } from 'rxjs';
-import { NzDrawerService } from 'ng-zorro-antd/drawer';
-import { NotificationService } from '../../../shared/service/notification/notification.service';
-import { CronService } from '../../../shared/service/cron/cron.service';
 
 @Component({
   selector: 'app-list',
@@ -17,11 +13,7 @@ export class ListComponent implements OnInit {
   @AutoUnsubscribe() list$: Observable<any> = this.store.select(CronSelector.list);
 
   constructor(
-      private datePipe: DatePipe,
-      private notify: NotificationService,
-      private cron: CronService,
       private store: Store,
-      private drawer: NzDrawerService,
   ) {
   }
 
